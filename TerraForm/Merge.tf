99401d9a211f2093b46d1521e1553fddb1eb5dab
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-23 00:59:17 +0530

    Update Merge.tf

--- TerraForm/Merge.tf
+++ TerraForm/Merge.tf
@@ -20,7 +20,7 @@ output "app_id" {
 
 #AppServicePlan
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_app_service_plan" "ASP" {
   name                = "api-appserviceplan-procmsds"
   location            = azurerm_resource_group.CM-Terraform-Test.location
   resource_group_name = azurerm_resource_group.CM-Terraform-Test.name
@@ -51,7 +51,7 @@ resource "azurerm_app_service" "webApp" {
   name                = "CM-Tf-test-webapp"
   location            = "${azurerm_resource_group.CM-Terraform-Test.location}"
   resource_group_name = "${azurerm_resource_group.CM-Terraform-Test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CM-Terraform-Test.id}"
+  app_service_plan_id = "${azurerm_app_service_plan.ASP.id}"
 
   site_config {
     dotnet_framework_version = "v4.0"
