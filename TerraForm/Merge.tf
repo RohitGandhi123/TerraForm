c534eb293b4308bd43731ffe07396f977feb3826
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-23 00:53:36 +0530

    Update Merge.tf

--- TerraForm/Merge.tf
+++ TerraForm/Merge.tf
@@ -45,16 +45,26 @@ resource "azurerm_storage_account" "Storage" {
   }
 }
 
-#FunctionApp
+#WebAPP
 
-resource "azurerm_app_service_plan" "FunctionApp" {
-  name                = "api-appserviceplan-pro"
-  location            = azurerm_resource_group.CM-Terraform-Test.location
-  resource_group_name = azurerm_resource_group.CM-Terraform-Test.name
-  kind                = "FunctionApp"
+resource "azurerm_app_service" "webApp" {
+  name                = "CM-Tf-test-webapp"
+  location            = "${azurerm_resource_group.CM-Terraform-Test.location}"
+  resource_group_name = "${azurerm_resource_group.CM-Terraform-Test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.CM-Terraform-Test.id}"
 
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
