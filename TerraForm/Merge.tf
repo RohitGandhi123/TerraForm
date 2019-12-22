c72c7f0b7ae8741e4047b2d750734fb4175d21d1
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-23 00:10:24 +0530

    Update Merge.tf

--- TerraForm/Merge.tf
+++ TerraForm/Merge.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "AppInsight" {
-  name     = "tf-test"
+resource "azurerm_resource_group" "CM-Terraform-Test" {
+  name     = "CM-Terraform-Test"
   location = "West Europe"
 }
 
 resource "azurerm_application_insights" "example" {
   name                = "tf-test-appinsights"
   location            = "West Europe"
-  resource_group_name = "${azurerm_resource_group.AppInsight.name}"
+  resource_group_name = "${azurerm_resource_group.CM-Terraform-Test.name}"
   application_type    = "web"
 }
 
@@ -17,13 +17,29 @@ output "instrumentation_key" {
 output "app_id" {
   value = "${azurerm_application_insights.example.app_id}"
 }
+
+#AppServicePlan
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "api-appserviceplan-procmsds"
+  location            = azurerm_resource_group.CM-Terraform-Test.location
+  resource_group_name = azurerm_resource_group.CM-Terraform-Test.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+#StorageAcccount
+
 resource "azurerm_resource_group" "Storage" {
   name     = "example-resourcescvgfhnbjy"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "cmmastervijaysgacc"
+  name                     = "storageaccountname"
   resource_group_name      = "${azurerm_resource_group.Storage.name}"
   location                 = "${azurerm_resource_group.Storage.location}"
   account_tier             = "Standard"
@@ -31,5 +47,19 @@ resource "azurerm_storage_account" "example" {
 
   tags = {
     environment = "staging"
+  }
+}
+
+#FunctionApp
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "api-appserviceplan-pro"
+  location            = azurerm_resource_group.CM-Terraform-Test.location
+  resource_group_name = azurerm_resource_group.CM-Terraform-Test.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
