b643cd9d42791050d5dd7a575b9b43db0dc47b95
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-23 19:45:56 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -3,8 +3,6 @@ resource "azurerm_resource_group" "CM-Terraform-Test" {
   location = "West Europe"
 }
 
-#AppServicePlan
-
 resource "azurerm_app_service_plan" "ASP" {
   name                = "api-appserviceplan-procmsds"
   location            = azurerm_resource_group.CM-Terraform-Test.location
@@ -18,16 +16,16 @@ resource "azurerm_app_service_plan" "ASP" {
 
 resource "azurerm_app_service" "webApp" {
 
-    count               = "${var.instance_count}"
-    name                = "${var.names}"
-    location            = "${azurerm_resource_group.CM-Terraform-Test.location}"
-    resource_group_name = "${azurerm_resource_group.CM-Terraform-Test.name}"
-    app_service_plan_id = "${azurerm_app_service_plan.ASP.id}"
+  count               = "${var.instance_count}"
+  name                = "${var.instance_tags}"
+  location            = "${azurerm_resource_group.CM-Terraform-Test.location}"
+  resource_group_name = "${azurerm_resource_group.CM-Terraform-Test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.ASP.id}"
 
-    tags = {
+   tags = {
     Name  = "${element(var.instance_tags, count.index)}"
     Batch = "5AM"
-    }
+  }
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -39,5 +37,5 @@ resource "azurerm_app_service" "webApp" {
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
+ }
 }
-
