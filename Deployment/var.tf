322b7762ee877f25832c960e15dc6bd75d4f31d0
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-23 17:56:06 +0530

    Update var.tf

--- Deployment/var.tf
+++ Deployment/var.tf
@@ -7,3 +7,8 @@ variable "instance_tags" {
   type = "list"
   default = ["Terraform-1", "Terraform-2"]
 }
+
+variable "names" {
+  type = "list"
+  default = ["CM-tf-Terraform-1", "CM-tf-Terraform-2"]
+}
