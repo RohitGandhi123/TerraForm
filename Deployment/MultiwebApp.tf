5c093325df526a6242a040d250b67b4c28202143
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-24 20:36:22 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,32 +1,28 @@
-resource "azurerm_resource_group" "CM-Terraform-Test" {
-  name     = "CM-Terraform-Test"
-  location = "West Europe"
+locals {
+    webappsperloc   = 3
 }
 
-#AppServicePlan
-
-resource "azurerm_app_service_plan" "ASP" {
-  name                = "api-appserviceplan-procmsds"
-  location            = azurerm_resource_group.CM-Terraform-Test.location
-  resource_group_name = azurerm_resource_group.CM-Terraform-Test.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_provider_type" "CM-Terraform-WebApp" {
-    count               = 3
-    name                = "cosmeticname-count.index"
-    location            = "${azurerm_resource_group.CM-Terraform-Test.location}"
-    resource_group_name = "${azurerm_resource_group.CM-Terraform-Test.name}"
-    tags                = "${azurerm_resource_group.CM-Terraform-Test.tags}"
-    app_service_plan_id = "${azurerm_app_service_plan.ASP.id}"
+resource "azurerm_app_service_plan" "free" {
+    count               = "${length(var.webapplocs)}"
+    name                = "plan-free-${var.webapplocs[count.index]}"
+    location            = "${var.webapplocs[count.index]}"
+    resource_group_name = "${azurerm_resource_group.webapps.name}"
+    tags                = "${azurerm_resource_group.webapps.tags}"
 
     kind                = "Linux"
+    reserved            = true
     sku {
         tier = "Free"
         size = "F1"
     }
+}
+
+resource "azurerm_app_service" "citadel" {
+    count               = "${ length(var.webapplocs) * local.webappsperloc }"
+    name                = "${format("webapp-%s-%02d-%s", random_string.webapprnd.result, count.index + 1, element(var.webapplocs, count.index))}"
+    location            = "${element(var.webapplocs, count.index)}"
+    resource_group_name = "${azurerm_resource_group.webapps.name}"
+    tags                = "${azurerm_resource_group.webapps.tags}"
+
+    app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
 }
