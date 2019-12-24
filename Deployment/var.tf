cd10330eea578c03f460c763eee8c776b7040703
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-24 14:34:34 +0530

    Update var.tf

--- Deployment/var.tf
+++ Deployment/var.tf
@@ -1,3 +1,4 @@
+
 variable "instance_count" {
   default = "2"
 }
@@ -5,4 +6,9 @@ variable "instance_count" {
 variable "instance_tags" {
   type = "list"
   default = ["Terraform-1", "Terraform-2"]
+}
+
+variable "names" {
+  type = "list"
+  default = ["CM-tf-Terraform-1", "CM-tf-Terraform-2"]
 }
