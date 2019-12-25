828a4792a4c3df0b5187598e1c915e400a0f2675
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-25 19:57:11 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -2,3 +2,22 @@ resource "azurerm_resource_group" "webapps"{
     name               ="webapps"
     location           ="west us"
 }
+
+locals {
+    webappsperloc   = 3
+}
+
+resource "azurerm_app_service_plan" "free" {
+    count               = "${length(var.webapplocs)}"
+    name                = "plan-free-${var.webapplocs[count.index]}"
+    location            = "${var.webapplocs[count.index]}"
+    resource_group_name = "${azurerm_resource_group.webapps.name}"
+    tags                = "${azurerm_resource_group.webapps.tags}"
+
+    kind                = "Linux"
+    reserved            = true
+    sku {
+        tier = "Free"
+        size = "F1"
+    }
+}
