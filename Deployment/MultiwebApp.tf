9b35d6aed76a0b64fe7a9361bbf8d64b0d8c45c4
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-25 18:55:35 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,28 +1,5 @@
-locals {
-    webappsperloc   = 3
-}
-
-resource "azurerm_app_service_plan" "free" {
-    count               = "${length(var.webappsperloc)}"
-    name                = "plan-free-${var.webappsperloc[count.index]}"
-    location            = "${var.webappsperloc[count.index]}"
-    resource_group_name = "${azurerm_resource_group.webapps.name}"
-    tags                = "${azurerm_resource_group.webapps.tags}"
-
-    kind                = "Linux"
-    reserved            = true
-    sku {
-        tier = "Free"
-        size = "F1"
-    }
-}
-
-resource "azurerm_app_service" "citadel" {
-    count               = "${ length(var.webappsperloc) * local.webappsperloc }"
-    name                = "${format("webapp-%s-%02d-%s", random_string.webapprnd.result, count.index + 1, element(var.webappsperloc, count.index))}"
-    location            = "${element(var.webappsperloc, count.index)}"
-    resource_group_name = "${azurerm_resource_group.webapps.name}"
-    tags                = "${azurerm_resource_group.webapps.tags}"
-
-    app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
+resource "azurerm_resource_group" "terraformresourcegp" {
+    count               = 3
+    name                = "cosmeticname-count.index"
+    location = "West Europe"
 }
