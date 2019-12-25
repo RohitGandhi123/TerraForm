c641b112ce1248d18f0066f2f6de70cb74c27735
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-25 19:53:11 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,11 +1,4 @@
-locals {
-    locations     = [ "eastus2", "uksouth", "centralindia" ]
-    default_prefix  = "rg-${var.tags["env"]}"
-    app_prefix      = "${var.app_prefix != "" ? var.app_prefix : local.default_prefix}"
-}
-
-resource "azurerm_resource_group" "terraformresourcegp" {
-    count               = "${length(local.locations)}"
-    name                = "${format("%s-%s-%s", local.app_prefix, random_string.webapprnd.result, local.locations[count.index])}"
-    location            = "${local.locations[count.index]}"
+resource "azurerm_resource_group" "webapps"{
+    name               ="webapps"
+    location           ="west us"
 }
