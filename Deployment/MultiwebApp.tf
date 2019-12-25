5d9814a1d368792dae4c3e00016b26d2229580ec
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-25 19:41:56 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,5 +1,11 @@
+locals {
+    locations     = [ "eastus2", "uksouth", "centralindia" ]
+    default_prefix  = "rg-${var.tags["env"]}"
+    app_prefix      = "${var.app_prefix != "" ? var.app_prefix : local.default_prefix}"
+}
+
 resource "azurerm_resource_group" "terraformresourcegp" {
-    count               = 3
-    name                = "cosmeticname-count.index"
-    location = "West Europe"
+    count               = "${length(local.locations)}"
+    name                = "${format("%s-%s-%s", local.app_prefix, random_string.webapprnd.result, local.locations[count.index])}"
+    location            = "${local.locations[count.index]}"
 }
