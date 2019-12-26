850f58ad9c0ba4176bda5be0a61b3a012275035f
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2019-12-26 13:00:56 +0530

    Update MultiwebApp.tf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "terraformresourcegp" {
     count               = 3
-    name                = ${var.name[count.index]}"
+    name                = "${var.name[count.index]}"
     locations           = "west us"
 }
