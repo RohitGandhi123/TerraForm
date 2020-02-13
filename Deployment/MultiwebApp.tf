eee04f25046706d7d06d69f0b397168e450536d2
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-13 18:36:46 +0530

    commentsd

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,12 +1,25 @@
 #AppServicePlan
 
 resource "azurerm_app_service_plan" "CRTM-SHELL-ASP" {
-  name                = "CRTM-Shell-Portal-ASPAS-SNCUS"
+  name                = "CRTM-Shell-Portal-ASP-SNCUS"
   location            = "North Central US"
-  resource_group_name = "CM-TRRA-QA-Master-RGasadsd"
+  resource_group_name = "Test"
 
   sku {
     tier = "Premium"
     size = "P1V2"
+  }
+}
+
+#WebAppPortal
+
+resource "azurerm_app_service" "CRTM-SHELL-PORTAL" {
+  name                = "CRTM-Shell-Portal-SNCUS"
+  location            = "North Central US"
+  resource_group_name = "CRTM-Shell-App-SNCUS-RG"
+  app_service_plan_id = "${azurerm_app_service_plan.CRTM-SHELL-ASP.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
   }
 }
