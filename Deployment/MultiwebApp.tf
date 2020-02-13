9b01b11dbd91b0f461f97b0fc982e870d3578348
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-13 17:43:38 +0530

    commenta

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,14 +1,7 @@
-#ResourceGroup
-
-resource "azurerm_resource_group" "Master-QA" {
-  name     = "CM-TRRA-QA-Master-RGasadsd"
-  location = "North Central US"
-}
-
 #AppInsight
 
 resource "azurerm_application_insights" "Master-QA-AppInsight" {
-  name                = "CM-TRRA-QA-Master-APP-INSIGHTSasweqwesds"
+  name                = "CM-TRRA-QA-Master-APP-INSIGHTSasweqwesdsasas"
   location            = "North Central US"
   resource_group_name = "${azurerm_resource_group.Master-QA.name}"
   application_type    = "web"
