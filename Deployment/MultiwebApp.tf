767fc377432a475f74afaa171d19d887aead0558
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-13 18:16:00 +0530

    commentrf

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -3,7 +3,7 @@
 resource "azurerm_app_service_plan" "CRTM-SHELL-ASP" {
   name                = "CRTM-Shell-Portal-ASPAS-SNCUS"
   location            = "North Central US"
-  resource_group_name = "Test"
+  resource_group_name = "CM-TRRA-QA-Master-RGasadsd"
 
   sku {
     tier = "Premium"
