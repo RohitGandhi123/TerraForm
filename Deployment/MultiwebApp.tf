3153155f9ee4c966d1f95a22e3fe3460536d2b69
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-13 20:08:53 +0530

    commitrg

--- Deployment/MultiwebApp.tf
+++ Deployment/MultiwebApp.tf
@@ -1,16 +1,16 @@
 
 #ResourceGroup
 
-resource "azurerm_resource_group" "CRTM-SHELL" {
-  name     = "CRTM-Shell-App-SNCUS-RG"
+resource "azurerm_resource_group" "CRTM-OTS" {
+  name     = "CRTM-OTS-App-SNCUS-RG"
   location = "North Central US"
 }
 #AppServicePlan
 
-resource "azurerm_app_service_plan" "CRTM-SHELL-ASP" {
-  name                = "CRTM-Shell-Portal-ASP-SNCUS"
-  location            = "${azurerm_resource_group.CRTM-SHELL.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-SHELL.name}"
+resource "azurerm_app_service_plan" "CRTM-OTS-ASP" {
+  name                = "CRTM-OTS-Portal-ASP-SNCUS"
+  location            = "${azurerm_resource_group.CRTM-OTS.location}"
+  resource_group_name = "${azurerm_resource_group.CRTM-OTS.name}"
 
   sku {
     tier = "Premuim"
@@ -20,11 +20,11 @@ resource "azurerm_app_service_plan" "CRTM-SHELL-ASP" {
 
 #WebAppPortal
 
-resource "azurerm_app_service" "CRTM-SHELL-PORTAL" {
-  name                = "CRTM-Shell-Portal-SNCUS"
-  location            = "${azurerm_resource_group.CRTM-SHELL.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-SHELL.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-SHELL-ASP.id}"
+resource "azurerm_app_service" "CRTM-OTS-PORTAL" {
+  name                = "CRTM-OTS-Portal-SNCUS"
+  location            = "${azurerm_resource_group.CRTM-OTS.location}"
+  resource_group_name = "${azurerm_resource_group.CRTM-OTS.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.CRTM-OTS-ASP.id}"
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -35,14 +35,43 @@ resource "azurerm_app_service" "CRTM-SHELL-PORTAL" {
 
 #WebAppPortalAPI
 
-resource "azurerm_app_service" "CRTM-SHELL-API" {
-  name                = "CRTM-Shell-API-SNCUS"
-  location            = "${azurerm_resource_group.CRTM-SHELL.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-SHELL.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-SHELL-ASP.id}"
+resource "azurerm_app_service" "CRTM-OTS-API" {
+  name                = "CRTM-OTS-API-SNCUS"
+  location            = "${azurerm_resource_group.CRTM-OTS.location}"
+  resource_group_name = "${azurerm_resource_group.CRTM-OTS.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.CRTM-OTS-ASP.id}"
 
   site_config {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
+}
+
+
+resource "azurerm_storage_account" "CRTM-OTS-STORAGE" {
+  name                     = "crtmotsfunc"
+  resource_group_name      = azurerm_resource_group.CRTM-OTS.name
+  location                 = azurerm_resource_group.CRTM-OTS.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "CRTM-OTS-FUNC-ASP" {
+  name                = "CRTM-OTS-Func-APP-ASP"
+  location            = azurerm_resource_group.CRTM-OTS.location
+  resource_group_name = azurerm_resource_group.CRTM-OTS.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                      = "CRTM-OTS-Func-APP"
+  location                  = azurerm_resource_group.CRTM-OTS.location
+  resource_group_name       = azurerm_resource_group.CRTM-OTS.name
+  app_service_plan_id       = azurerm_app_service_plan.CRTM-OTS-FUNC-ASP.id
+  storage_connection_string = azurerm_storage_account.CRTM-OTS-STORAGE.primary_connection_string
 }
