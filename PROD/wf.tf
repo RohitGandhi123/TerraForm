80cfc46aa1c4230bda6bf6212d3eca727bd22309
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-14 16:46:41 +0530

    bb

--- PROD/wf.tf
+++ PROD/wf.tf
@@ -17,7 +17,7 @@ resource "azurerm_app_service" "CRTM-OTS-PORTAL" {
   name                = "CRTM-OTS-Portal-SSCUS"
   location            = "North Central US"
   resource_group_name = "CRTM-OTS-App-SSCUS-RG"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-OTS-ASP.id}"
+  app_service_plan_id = "azurerm_app_service_plan.CRTM-OTS-ASP.id"
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -32,7 +32,7 @@ resource "azurerm_app_service" "CRTM-OTS-API" {
   name                = "CRTM-OTS-API-SSCUS"
   location            = "North Central US"
   resource_group_name = "CRTM-OTS-App-SSCUS-RG"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-QTS-ASP.id}"
+  app_service_plan_id = "azurerm_app_service_plan.CRTM-QTS-ASP.id"
 
   site_config {
     dotnet_framework_version = "v4.0"
