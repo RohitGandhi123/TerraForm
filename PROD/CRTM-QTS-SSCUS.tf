6384bc8173b1ec4697682b4c8b20853c67e7c942
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-14 19:31:01 +0530

    uio

--- PROD/CRTM-QTS-SSCUS.tf
+++ PROD/CRTM-QTS-SSCUS.tf
@@ -1,47 +1,27 @@
-#ResourceGroup
-
-resource "azurerm_resource_group" "CRTM-OTS" {
-  name     = "CRTM-OTS-App-SSCUS-RG"
-  location = "South Central US"
+resource "azurerm_storage_account" "CRTM-OTS-STORAGE" {
+  name                     = "crtmotsfuncsscus"
+  resource_group_name      = "CRTM-OTS-App-SSCUS-RG"
+  location                 = "South Central US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
-#AppServicePlan
 
-resource "azurerm_app_service_plan" "CRTM-OTS-ASP" {
-  name                = "CRTM-OTS-Portal-ASP-SSCUS"
-  location            = "${azurerm_resource_group.CRTM-OTS.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-OTS.name}"
+resource "azurerm_app_service_plan" "CRTM-OTS-FUNC-ASP" {
+  name                = "CRTM-OTS-Func-APP-ASP-SSCUS"
+  location            = "South Central US"
+  resource_group_name = "CRTM-OTS-App-SSCUS-RG"
+  kind                = "FunctionApp"
 
   sku {
-    tier = "Premuim"
-    size = "P1V2"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
 
-#WebAppPortal
-
-resource "azurerm_app_service" "CRTM-OTS-PORTAL" {
-  name                = "CRTM-OTS-Portal-SSCUS"
-  location            = "${azurerm_resource_group.CRTM-OTS.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-OTS.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-OTS-ASP.id}"
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-}
-
-
-#WebAppPortalAPI
-
-resource "azurerm_app_service" "CRTM-OTS-API" {
-  name                = "CRTM-OTS-API-SSCUS"
-  location            = "${azurerm_resource_group.CRTM-OTS.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-OTS.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-OTS-ASP.id}"
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
+resource "azurerm_function_app" "CRTM-OTS-FUNC-APP" {
+  name                      = "CRTM-OTS-Func-APP-SSCUS"
+  location                  = "South Central US"
+  resource_group_name       = "CRTM-OTS-App-SSCUS-RG"
+  app_service_plan_id       = azurerm_app_service_plan.CRTM-OTS-FUNC-ASP.id
+  storage_connection_string = azurerm_storage_account.CRTM-OTS-STORAGE.primary_connection_string
 }
