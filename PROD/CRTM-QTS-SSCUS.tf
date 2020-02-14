03e11629648948ec8e91ab883ce2816f7a5b87b0
RohitGandhi123 <53550943+RohitGandhi123@users.noreply.github.com>
2020-02-14 18:59:30 +0530

    id

--- PROD/CRTM-QTS-SSCUS.tf
+++ PROD/CRTM-QTS-SSCUS.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "CRTM-QTS" {
 
 resource "azurerm_app_service_plan" "CRTM-QTS-ASP" {
   name                = "CRTM-QTS-Portal-ASP-SSCUS"
-  location            = "${azurerm_resource_group.CRTM-QTS.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-QTS.name}"
+  location            = azurerm_resource_group.CRTM-QTS.location
+  resource_group_name = azurerm_resource_group.CRTM-QTS.name
 
   sku {
     tier = "Premuim"
@@ -22,9 +22,9 @@ resource "azurerm_app_service_plan" "CRTM-QTS-ASP" {
 
 resource "azurerm_app_service" "CRTM-QTS-PORTAL" {
   name                = "CRTM-QTS-Portal-SSCUS"
-  location            = "${azurerm_resource_group.CRTM-QTS.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-QTS.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-QTS-ASP.id}"
+  location            = azurerm_resource_group.CRTM-QTS.location
+  resource_group_name = azurerm_resource_group.CRTM-QTS.name
+  app_service_plan_id = azurerm_app_service_plan.CRTM-QTS-ASP.id
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -37,9 +37,9 @@ resource "azurerm_app_service" "CRTM-QTS-PORTAL" {
 
 resource "azurerm_app_service" "CRTM-QTS-API" {
   name                = "CRTM-QTS-API-SSCUS"
-  location            = "${azurerm_resource_group.CRTM-QTS.location}"
-  resource_group_name = "${azurerm_resource_group.CRTM-QTS.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.CRTM-QTS-ASP.id}"
+  location            = azurerm_resource_group.CRTM-QTS.location
+  resource_group_name = azurerm_resource_group.CRTM-QTS.name
+  app_service_plan_id = azurerm_app_service_plan.CRTM-QTS-ASP.id
 
   site_config {
     dotnet_framework_version = "v4.0"
